3e770d693718d7dea6bab12eb06ff3d4d2751389
Stuart Olivera <stuart@stuartolivera.com>
2016-05-02 22:36:04 -0400

    Added hostname to herrd auth deep link

--- app/controllers/herrd/routes_controller.rb
+++ app/controllers/herrd/routes_controller.rb
@@ -43,7 +43,7 @@ module Herrd
       end
 
       def herrd_login_url
-        "herrd://auth?api_key=#{api_key}&username=#{current_user.username}"
+        "herrd://auth?api_key=#{api_key}&username=#{current_user.username}&hostname=#{GlobalSetting.hostname}"
       end
 
       def herrd_redirect_to_login_if_required
